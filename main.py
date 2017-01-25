b5975383d189dfc1b6239998980265d841a41093
jpoirier55 <jake.poirier55@gmail.com>
2017-01-25 23:57:11 +0000

    Working serial handlers for both send adn receive, UDP server working

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import SocketServer
 import sys, os
-sys.path.append(os.path.abspath("."))
 import uart
+import argparse
 
 class UDPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print "{} wrote:".format(self.client_address[0])
-        print data
-        uart.process_command(data)
+        # print "{} wrote:".format(self.client_address[0])
+        # print data
+        # print self.finish
+        # print self.server
+        # print self.request
+        command_handler = uart.CommandHandler(data)
+        command_handler.process_command()
 
 if __name__ == '__main__':
-    HOST,PORT = '0.0.0.0', 12345
-    server = SocketServer.UDPServer((HOST, PORT), UDPHandler)
+    parser = argparse.ArgumentParser(description='Provide port and host for UDP server')
+    parser.add_argument('--h', '--HOST', default='0.0.0.0', help='Host ipv4 address (default 0.0.0.0)')
+    parser.add_argument('--p', '--PORT', default=65000, help='Port (default 65000)')
+
+    args = parser.parse_args()
+    HOST,PORT = args.h, args.p
+    server = SocketServer.UDPServer((HOST, int(PORT)), UDPHandler)
     server.serve_forever()
--- uart.py
+++ uart.py
@@ -1,48 +1,60 @@
 import serial
 import json
 
-uart1 = '/dev/ttyO1'
-uart2 = '/dev/ttyO2'
-uart4 = '/dev/ttyO4'
-uart5 = '/dev/ttyO5'
-uart0 = '/dev/ttyO0'
-baud_rate = 9600
+
+class SerialSendHandler:
+    def __init__(self, baudrate=115200, timeout=5):
+        self.baudrate = baudrate
+        self.timeout = timeout
+
+    def open(self):
+        self.port.open()
+
+    def close(self):
+        self.port.close()
+
+    def test_connection(self):
+        if self.port.is_open():
+            return True
+
+    def send_uart(self, json_command, uart_send):
+        print 'sending: ', json.dumps(json_command), uart_send, " ", self.baudrate
+
+        ser = serial.Serial(uart_send, baudrate=self.baudrate, timeout=self.timeout)
+        ser.write(json.dumps(json_command) + '\n')
+        ser.close()
 
 
-def process_command(command):
-    if len(command) > 1:
-        for json_c in json.loads(command):
+class CommandHandler:
+    def __init__(self, command):
+        self.uarts = ['/dev/ttyO1', '/dev/ttyO2', '/dev/ttyO4', '/dev/ttyO5']
+        self.json_command = json.loads(command)
+        self.serial_handler = SerialSendHandler()
+
+    def calculate_controller(self, seat_id):
+        # modid = seat_id%4
+        #uart_send = self.uarts[0]
+        if seat_id < 51:
+            uart_send = self.uarts[0]
+        elif seat_id >= 51 and seat_id < 102:
+            uart_send = self.uarts[1]
+        elif seat_id >= 102 and seat_id < 153:
+            uart_send = self.uarts[2]
+        else:
+            uart_send = self.uarts[3]
+        #uart_send = self.uarts[str(modid)]
+        return uart_send
+
+    def build_uart_command(self, seat_id, state):
+        uart_seat = seat_id % 60
+        json = {'seat_id': uart_seat,
+                'state': state}
+        return json
+
+    def process_command(self):
+        for json_c in self.json_command:
             seat_id = json_c['seat_id']
             state = json_c['state']
-
-            uart_send = calculate_controller(seat_id)
-            json_command = build_uart_command(seat_id, state)
-            send_uart(json_command, uart_send)
-    else:
-        json_c = json.loads(command)
-        seat_id = json_c['seat_id']
-        state = json_c['state']
-
-        uart_send = calculate_controller(seat_id)
-        json_command = build_uart_command(seat_id, state)
-        send_uart(json_command, uart_send)
-
-def calculate_controller(seat_id):
-    if seat_id < 60:
-        uart_send = uart1
-    if seat_id > 60:
-        uart_send = uart4
-    return uart_send
-
-def build_uart_command(seat_id, state):
-    uart_seat = seat_id % 60
-    json = {'seat_id': uart_seat,
-            'state': state}
-    return json
-
-
-def send_uart(json_command, uart_send):
-    print 'sending: ', uart_send, " ", baud_rate
-    ser = serial.Serial(uart_send, baud_rate)
-    ser.write(json.dumps(json_command))
-    ser.close()
+            uart_send = self.calculate_controller(seat_id)
+            json_command = self.build_uart_command(seat_id, state)
+            self.serial_handler.send_uart(json_command, uart_send)
