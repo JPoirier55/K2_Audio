3e0d7b2f5dd248afc05717188602aa57be2d53ef
jpoirier55 <jake.poirier55@gmail.com>
2017-02-21 22:53:45 +0000

    Left over changes not committed

---
+++ dev/tcp_multi.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+python /home/debian/K2_Audio/dev/packet_test.py &
+python /home/debian/K2_Audio/dev/packet_test.py &
+python /home/debian/K2_Audio/dev/packet_test.py &
+python /home/debian/K2_Audio/dev/packet_test.py &
--- tcp_server.py
+++ tcp_server.py
@@ -16,58 +16,83 @@ date           programmer         modification
 2/1/17          JDP                original
 """
 
-import SocketServer
-import socket
 import json
 from message_utils import MessageHandler, error_response
 import argparse
 import Queue
-from threading import Thread
-import threading
-DEBUG = True
-import socket
 import threading
 import SocketServer
 import serial
-import time
+DEBUG = True
 
-ser = serial.Serial('/dev/ttyO5', 115200)
 
-q = Queue.Queue()
+UART_PORTS = ['/dev/ttyO1', '/dev/ttyO2', '/dev/ttyO4', '/dev/ttyO5']
+GLOBAL_QUEUE = Queue.Queue()
 
 
 def worker():
+    """
+    Threads for each TCP connection that is made.
+    These workers retrieve from the global queue
+    in a non-blocking form which is a characteristic
+    of the python FIFO Queue.
+
+    The messages that get pulled from the queue
+    are tuples with form (uart_command, uart_port)
+    @return: None
+    """
     while True:
-        item = q.get()
-        time.sleep(1)
-        ser.write(item + "\n")
+        item = GLOBAL_QUEUE.get()
+        ser = serial.Serial(item[1], 115200)
+        ser.write(str(item[0]) + "\n")
 
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
+        """
+        TCP Socket Server builtin function to handle
+        incoming packets. Inserts messages onto the
+        threaded global queue GLOBAL_QUEUE object along
+        with the uart port set by the incoming component_id
+        @return: None
+        """
         self.data = self.request.recv(1024)
-        cur_thread = threading.current_thread()
 
         json_data = json.loads(self.data)
         if all(key in json_data for key in ("action", "category", "component", "component_id", "value")):
             msg = MessageHandler(json_data)
-            response, uart_command = msg.process_command()
-            q.put(uart_command)
+            response, (uart_command, uart_port) = msg.process_command()
+            if not uart_port or not uart_command:
+                self.request.sendall(json.dumps(response) + '\n')
+                return
+            elif uart_port == "ALL":
+                for port in UART_PORTS:
+                    GLOBAL_QUEUE.put((uart_command, port))
+            elif uart_port == "ARRAY":
+                for uart_port, single_uart_command in uart_command.iteritems():
+                    GLOBAL_QUEUE.put((json.dumps(single_uart_command), uart_port))
+            else:
+                GLOBAL_QUEUE.put((uart_command, uart_port))
             self.request.sendall(json.dumps(response))
         else:
-            self.request.sendall(json.dumps(error_response(1)) + '\n')
+            self.request.sendall(error_response(1)[0] + '\n')
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    """
+    Built in for Threaded SocketServer
+    """
     pass
 
-
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "0.0.0.0", 65000
+    parser = argparse.ArgumentParser(description='Provide port and host for TCP server')
+    parser.add_argument('--h', '--HOST', default='0.0.0.0', help='Host ipv4 address (default 0.0.0.0)')
+    parser.add_argument('--p', '--PORT', default=65000, help='Port (default 65000)')
 
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    args = parser.parse_args()
+    HOST, PORT = args.h, args.p
+    server = ThreadedTCPServer((HOST, int(PORT)), ThreadedTCPRequestHandler)
     ip, port = server.server_address
 
     # Start a thread with the server -- that thread will then start one
@@ -79,73 +104,9 @@ if __name__ == "__main__":
     server_thread.start()
     queue_thread.daemon = True
     queue_thread.start()
-    print "Server loop running in thread:", server_thread.name
-    print 'queue_thread running in thread: ', queue_thread.name
 
-    # client(ip, port, "Hello World 1")
-    # client(ip, port, "Hello World 2")
-    # client(ip, port, "Hello World 3")
+    if DEBUG:
+        print "Server loop running in thread:", server_thread.name
+        print 'Global Queue running in thread: ', queue_thread.name
 
     server.serve_forever()
-
-
-#
-
-#
-# QUEUE_HANDLER = QueueHandler()
-#
-# def queue_runner(queue_handler):
-#     while True:
-#         msg = queue_handler.dequeue()
-#         if msg is not None:
-#             print 'ok'
-#         else:
-#             print 'not okay'
-
-
-
-# class MyTCPHandler(SocketServer.BaseRequestHandler):
-#
-#     def setup(self):
-#         print 'Setting up socket server and Queue...'
-#         worker = Thread(target=queue_runner, args=QUEUE_HANDLER)
-#         worker.setDaemon(True)
-#         worker.start()
-#
-#     def handle(self):
-#         """
-#         Main handler method for TCP server
-#         @return: None
-#         """
-#         self.data = self.request.recv(1024).strip()
-#
-#         if DEBUG:
-#             print "{} wrote:".format(self.client_address[0])
-#         print self.data
-#
-#         try:
-#             json_data = json.loads(self.data)
-#             if all(key in json_data for key in ("action", "category", "component", "component_id", "value")):
-#                 msg = MessageHandler(json_data)
-#                 print 'Adding msg to queue:', json_data
-#                 QUEUE_HANDLER.enqueue(msg, self.request)
-#                 #response = msg.process_command()
-#                 #self.request.sendall(json.dumps(response))
-#             else:
-#                 self.request.sendall(json.dumps(error_response(1)) + '\n')
-#
-#         except Exception as e:
-#             if DEBUG:
-#                 print e
-#             self.request.sendall(json.dumps(error_response(3)) + '\n')
-#
-#
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description='Provide port and host for TCP server')
-#     parser.add_argument('--h', '--HOST', default='0.0.0.0', help='Host ipv4 address (default 0.0.0.0)')
-#     parser.add_argument('--p', '--PORT', default=65000, help='Port (default 65000)')
-#
-#     args = parser.parse_args()
-#     HOST, PORT = args.h, args.p
-#     server = SocketServer.TCPServer((HOST, int(PORT)), MyTCPHandler)
-#     server.serve_forever()
