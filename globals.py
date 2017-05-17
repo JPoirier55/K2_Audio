20c833343ddaf2bd1ddaf382879dd499cc0b5216
jpoirier55 <jake.poirier55@gmail.com>
2017-05-17 17:00:15 +0000

    Flip all rts/cts to what they should be, add in bools for setting tcp server and startup

--- globals.py
+++ globals.py
@@ -14,29 +14,37 @@ date           programmer         modification
 
 import Adafruit_BBIO.GPIO as GPIO
 
-GPIO.setup('P8_39', GPIO.OUT)
-GPIO.setup('P8_41', GPIO.OUT)
-GPIO.setup('P8_43', GPIO.OUT)
-GPIO.setup('P8_45', GPIO.OUT)
+GPIO.setup('P8_39', GPIO.IN)
+GPIO.setup('P8_41', GPIO.IN)
+GPIO.setup('P8_43', GPIO.IN)
+GPIO.setup('P8_45', GPIO.IN)
 
-GPIO.setup('P8_40', GPIO.IN)
-GPIO.setup('P8_42', GPIO.IN)
-GPIO.setup('P8_44', GPIO.IN)
-GPIO.setup('P8_46', GPIO.IN)
+GPIO.setup('P8_40', GPIO.OUT)
+GPIO.setup('P8_42', GPIO.OUT)
+GPIO.setup('P8_44', GPIO.OUT)
+GPIO.setup('P8_46', GPIO.OUT)
 
-GPIO.output("P8_39", GPIO.LOW)
-GPIO.output("P8_41", GPIO.LOW)
-GPIO.output("P8_43", GPIO.LOW)
-GPIO.output("P8_45", GPIO.LOW)
+GPIO.output("P8_40", GPIO.LOW)
+GPIO.output("P8_42", GPIO.LOW)
+GPIO.output("P8_44", GPIO.LOW)
+GPIO.output("P8_46", GPIO.LOW)
 
-RTS_GPIOS = ['/sys/class/gpio/gpio71/value', '/sys/class/gpio/gpio73/value',
-             '/sys/class/gpio/gpio75/value', '/sys/class/gpio/gpio77/value']
-GPIO_EDGE_FDS = ['/sys/class/gpio/gpio71/edge', '/sys/class/gpio/gpio73/edge',
-                 '/sys/class/gpio/gpio75/edge', '/sys/class/gpio/gpio77/edge']
+GPIO.add_event_detect("P8_39", GPIO.RISING)
+GPIO.add_event_detect("P8_41", GPIO.RISING)
+GPIO.add_event_detect("P8_43", GPIO.RISING)
+GPIO.add_event_detect("P8_45", GPIO.RISING)
 
-CTS_GPIOS = ['P8_45', 'P8_43', 'P8_41', 'P8_39']
+
+RTS_GPIOS = ['/sys/class/gpio/gpio70/value', '/sys/class/gpio/gpio72/value',
+             '/sys/class/gpio/gpio74/value', '/sys/class/gpio/gpio76/value']
+GPIO_EDGE_FDS = ['/sys/class/gpio/gpio70/edge', '/sys/class/gpio/gpio72/edge',
+                 '/sys/class/gpio/gpio74/edge', '/sys/class/gpio/gpio76/edge']
+
+CTS_GPIOS = ['P8_46', 'P8_44', 'P8_42', 'P8_40']
 
 DEBUG = True
+TCP_ON = False
+
 DEV_UART_PORTS = ['/dev/ttyO1', '/dev/ttyO2']
 UART_PORTS = ['/dev/ttyO1', '/dev/ttyO2', '/dev/ttyO4', '/dev/ttyO5']
 xUART_PORTS = ['/dev/ttyO1', '/dev/ttyO1', '/dev/ttyO1', '/dev/ttyO1']
@@ -46,20 +54,12 @@ MICRO_ERR = bytearray.fromhex('E8018069EE')
 BB_ACK = bytearray('E8018069EE')
 DSP_SERVER_IP = '192.168.255.88'
 DSP_SERVER_PORT = 65000
-MICRO_STATUS = 'E80231001BEE'
-ALL_LEDS = 'E80240F80123EE'
-ALL_LEDS_OFF = 'E80240F80022EE'
+MICRO_STATUS = bytearray.fromhex('E80231001BEE')
+ALL_LEDS = bytearray.fromhex('E80240F80123EE')
+ALL_LEDS_OFF = bytearray.fromhex('E80240F80022EE')
 SERIAL_TIMEOUT = 1
 SERIAL_BAUDRATE = 115200
 SOCKET_TIMEOUT = 1
-
-FIRMWARE = {'1': '001',
-            '2': '002',
-            '3': '003',
-            '4': '004',
-            '5': '005',
-            '6': '006',
-            '7': '007'}
 
 ERROR_DESCS = ['Invalid category or component.',
                'State (parameter) out of range',
